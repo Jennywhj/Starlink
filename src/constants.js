1ffaf99a8d6e26df53d5d6b76ff616ba5f7c4271
Jennywhj <104221434+Jennywhj@users.noreply.github.com>
2022-12-09 14:34:19 -0500

    init commit

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "PGW5JZ-Z8SDC6-73WVUX-4V9U";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const BASE_URL = "";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
